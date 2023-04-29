a9c79546a5cd5e445e5982bf8897d4c7fccaa574
j1642 <60148902+j1642@users.noreply.github.com>
2023-04-29 12:22:16 -0400

    ENH: Parse if/else blocks

--- parser/parser.go
+++ parser/parser.go
@@ -228,6 +228,14 @@ func (p *Parser) parseIfExpression() ast.Expression {
 		return nil
 	}
 	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+		expression.Alternative = p.parseBlockStatement()
+	}
 	return expression
 }
 
