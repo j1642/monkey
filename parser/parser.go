e8f11ff7c0dcc6ddec1d7d095bec0e6116290848
j1642 <60148902+j1642@users.noreply.github.com>
2023-05-25 13:14:30 -0400

    ENH: Parse array index expressions (array[0])

--- parser/parser.go
+++ parser/parser.go
@@ -18,6 +18,7 @@ const (
 	PRODUCT
 	PREFIX // -x, !x
 	CALL   // function call
+    INDEX
 )
 
 var precedences = map[token.TokenType]int{
@@ -30,6 +31,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+    token.LBRACKET: INDEX,
 }
 
 type (
@@ -78,6 +80,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	return p
 }
@@ -343,6 +346,17 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 		return nil
 	}
 	return list
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+    exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+    p.nextToken()
+    exp.Index = p.parseExpression(LOWEST)
+
+    if !p.expectPeek(token.RBRACKET) {
+        return nil
+    }
+    return exp
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
